a2bdf06ab8e1186bcc3cafe793a3f1deee8074bd
Christian Georgeou <cgeorgeou@hotmail.com>
2018-07-18 20:39:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function ternaryCheckCity(city) {
 }
 
 function switchOnCharmFromTip(tip) {
-switch ('generous')
+switch (tip = 'generous')
   { case 'generous':
     tip ='Thank you so much.';
     break;
